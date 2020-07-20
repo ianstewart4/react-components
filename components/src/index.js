5bf730243a706bfec16ca9d612e04ab925bf5762
Ian Stewart <ianstewart4@outlook.com>
2020-07-19 20:50:52 -0500

    add commentdetail and approval card

--- components/src/index.js
+++ components/src/index.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <ApprovalCard />
+            <CommentDetail 
+                author="Bryan" 
+                timeAgo="Today at 4:45PM" 
+                content="Great job!" 
+                avatar={faker.image.avatar()}/>
+            <CommentDetail 
+                author="Alex" 
+                timeAgo="Today at 2:35AM" 
+                content="Looking forward to your next post!" 
+                avatar={faker.image.avatar()}/>
+            <CommentDetail 
+                author="Sam" 
+                timeAgo="Yesterday at 4:45PM" 
+                content="Another good one!" 
+                avatar={faker.image.avatar()} />
         </div>
     );
 }
