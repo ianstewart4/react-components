43bae4b9c9463a0038a67cea87c1e68db4b48613
Ian Stewart <ianstewart4@outlook.com>
2020-07-26 21:06:22 -0500

    adding final components

--- components/src/index.js
+++ components/src/index.js
@@ -7,22 +7,33 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard />
-            <CommentDetail 
-                author="Bryan" 
-                timeAgo="Today at 4:45PM" 
-                content="Great job!" 
-                avatar={faker.image.avatar()}/>
-            <CommentDetail 
-                author="Alex" 
-                timeAgo="Today at 2:35AM" 
-                content="Looking forward to your next post!" 
-                avatar={faker.image.avatar()}/>
-            <CommentDetail 
-                author="Sam" 
-                timeAgo="Yesterday at 4:45PM" 
-                content="Another good one!" 
-                avatar={faker.image.avatar()} />
+            <ApprovalCard>
+                <div>
+                    <h4>Warning!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Bryan"
+                    timeAgo="Today at 4:45PM"
+                    content="Great job!"
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 2:35AM"
+                    content="Looking forward to your next post!"
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Yesterday at 4:45PM"
+                    content="Another good one!"
+                    avatar={faker.image.avatar()} />
+            </ApprovalCard>
         </div>
     );
 }
